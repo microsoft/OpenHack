27d0c33d6137d596b25ff014ae0ee0eecbc71d84
dwnatwick <dwnatwick@comcast.net>
2020-08-31 12:52:31 -0400

    local updates

--- byos/modern-data-warehousing/deploy/BYOS-deployAll.ps1
+++ byos/modern-data-warehousing/deploy/BYOS-deployAll.ps1
@@ -7,10 +7,11 @@ param(
     [string]$Location = "eastus",
     [securestring]$SqlAdminLoginPassword,
     [securestring]$VMAdminPassword,
-    [securestring]$BackupStorageContainerSAScd
+    [securestring]$BackupStorageContainerSAS
 )
 $teamCount = Read-Host "How many teams are hacking?";
 $region = Read-Host "What Region Resources be deployed to (i.e. centralus, southcentralus, japaneast, etc)?";
+
 
 for ($i = 1; $i -le $teamCount; $i++)
 {
@@ -30,5 +31,5 @@ for ($i = 1; $i -le $teamCount; $i++)
     if (!$resourceGroup) {
         $resourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $region
     }
-    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -Location $region -TemplateFile $DeploymentTemplateFile -TemplateParameterFile $DeploymentParameterFile -SqlAdminLoginPassword $SqlAdminLoginPassword -VMAdminPassword $VMAdminPassword -BackupStorageContainerSAS $BackupStorageContainerSAS -AsJob    
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -Location $region -TemplateFile $DeploymentTemplateFile -TemplateParameterFile $DeploymentParameterFile -sqlAdminLoginPassword $SqlAdminLoginPassword -VMAdminPassword $VMAdminPassword -BackupStorageContainerSAS $BackupStorageContainerSAS -AsJob    
 }
